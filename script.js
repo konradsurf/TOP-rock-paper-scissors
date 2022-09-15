faee5b21787ea6b31e64a030617e095889377074
konradkarwowski <konrad.bipc@gmail.com>
2022-09-15 15:01:22 -0700

    Created function to play five round game

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
-// create variable for player's choice playerSelection
-let player1 = 'scissors';
-let player2 = 'rock';
+
+// call function to play 5 rounds
+game();
 
 
-// prompt player for their choice
-player1 = prompt("Please enter rock, paper or scissors:");
-// convert player's input into all lowercase to ensure match
-// this could be combined in line above where we prompt user for their choice
-player1 = player1.toLowerCase();
+// function to play 5 round game
+function game() {
+  // create variables for player and computer choice
+  let player1;
+  let player2;
+  // initialize scores
+  let playerScore = 0;
+  let computerScore = 0;
 
-// output who won to console by calling playRound function
-console.log(playRound(player1, getComputerChoice()));
-
-
-
-
+  // loop through game five times
+  for (let i = 0; i < 5; i++) {
+    // prompt player for their choice
+    player1 = prompt("Please enter rock, paper or scissors:");
+    // convert player's input into all lowercase to ensure match
+    // this could be combined in line above where we prompt user for their choice
+    player1 = player1.toLowerCase();
+    // get the computer's choice
+    player2 = getComputerChoice();
+    // output who won to console by calling playRound function
+    if (playRound(player1, player2) == 'win') {
+      // display winning message
+      console.log("You win this round!")
+      // increment player's score
+      playerScore += 1;
+      console.log(`Player score is: ${playerScore} ,  Computer's score is: ${computerScore} `);
+    }
+    else if (playRound(player1, player2) == 'lose') {
+      console.log("You lose this round :(")
+      computerScore += 1; 
+      console.log(`Player score is: ${playerScore} ,  Computer's score is: ${computerScore} `);
+    }
+    else if (playRound(player1, player2) == 'tie') {
+      console.log("You tied this round") 
+      console.log(`Player score is: ${playerScore} ,  Computer's score is: ${computerScore} `);
+    }
+  }
+  // output result of the entire five rounds to console
+  if (playerScore > computerScore) {
+    console.log(`You won the game. Score was ${playerScore} to ${computerScore}`);
+  }
+  else if (playerScore < computerScore) {
+    console.log(`You lost the game. Score was ${playerScore} to ${computerScore}`);
+  }
+  else if (playerScore == computerScore) {
+    console.log(`Tie game. Score was ${playerScore} to ${computerScore}`);
+  }
+} 
 
 // function to get computer's choiceand initialize computerSelection
 function getComputerChoice() {
@@ -27,39 +62,40 @@ function getComputerChoice() {
 }
 
 
-// function that takes choices and returns a string indicating who won
+// function that takes choices and returns a string indicating if player won or lost
 // could also have nested these - player selection, then an if statement within it with the 
 // three choices that computer could pick
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    return "You win!";
+    return "win";
   }
   else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-    return "Computer wins :(";
+    return "lose";
   }
   else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-    return "Tie game."
+    return "tie";
   }
   else if (playerSelection == 'rock' && computerSelection == 'paper') {
-    return "Computer wins :(";
+    return "lose";
   }
   else if (playerSelection == 'paper' && computerSelection == 'paper') {
-    return "Tie game.";
+    return "tie";
   }
   else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-    return "You win!";
+    return "win";
   }
   else if (playerSelection == 'rock' && computerSelection == 'rock') {
-    return "Tie game.";
+    return "tie";
     }
   else if (playerSelection == 'paper' && computerSelection == 'rock') {
-    return "You win!";
+    return "win";
   }
   else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-    return "Computer wins :(";
+    return "lose";
   }
   else {
     return "An error occured";
   }
 }
-// function to initialize computer's choice to random choice
+
+
